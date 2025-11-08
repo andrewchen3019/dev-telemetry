c8696e5c8edd79707c047ee7db24c153e44884f7
Awesome Andrew <reallyAwesomeAndrew@gmail.com>
2025-11-07 23:38:23 -0500

    added .ino file, modified overview

--- src/application/pages/DevicePages/OverviewPage.tsx
+++ src/application/pages/DevicePages/OverviewPage.tsx
@@ -10,55 +10,73 @@ import { Card } from '@blueprintjs/core'
 import { Composition } from 'atomic-layout'
 import { IntervalRequester } from '@electricui/components-core'
 import { LightBulb } from '../../components/LightBulb'
-import { Data } from '../../components/Data'
+// import { Data } from '../../components/Data'
 import { useMessageDataSource } from '@electricui/core-timeseries'
 import React from 'react'
 import { RouteComponentProps } from '@reach/router'
 import { Slider } from '@electricui/components-desktop-blueprint'
 
 const layoutDescription = `
-  Chart Chart
-  Light Slider
+  ChartSpeed ChartBattery ChartLED
+  Light      Slider       Slider
 `
 
 export const OverviewPage = (props: RouteComponentProps) => {
-  const ledStateDataSource = useMessageDataSource('led_state')
-
+  const ledStateDataSource = useMessageDataSource('led_state');
+  const batteryDataSource = useMessageDataSource('battery');
+  const speedDataSource = useMessageDataSource('speed');
   return (
     <React.Fragment>
-      <IntervalRequester interval={50} messageIDs={['led_state']} />
+      <IntervalRequester interval={50} messageIDs={['led_state','battery','speed']} />
 
       <Composition areas={layoutDescription} gap={10} autoCols="1fr">
         {Areas => (
           <React.Fragment>
-             <Card>
-                <div style={{ textAlign: 'center', marginBottom: '1em' }}>
-                  <b>Vehicle Speed and Battery Efficiency</b>
-                </div>
-                <Data/>
-              </Card>
-              
-{/*             
-            <Areas.Chart>
+            <Areas.ChartSpeed>
               <Card>
                 <div style={{ textAlign: 'center', marginBottom: '1em' }}>
-                  <b>LED State</b>
+                  <b>Speed</b>
                 </div>
                 <ChartContainer>
-                  <LineChart dataSource={ledStateDataSource} />
+                  <LineChart key="speed" dataSource={speedDataSource} />
                   <RealTimeDomain window={10000} />
                   <TimeAxis />
                   <VerticalAxis />
                 </ChartContainer>
               </Card>
-            </Areas.Chart> */}
-{/* 
+            </Areas.ChartSpeed>
+
+            <Areas.ChartBattery>
+              <Card>
+                <div style={{ textAlign: 'center', marginBottom: '1em' }}>
+                  <b>Battery Efficiency</b>
+                </div>
+                <ChartContainer>
+                  <LineChart key="battery" dataSource={batteryDataSource} />
+                  <RealTimeDomain window={10000} />
+                  <TimeAxis />
+                  <VerticalAxis />
+                </ChartContainer>
+              </Card>
+            </Areas.ChartBattery>
+
+            <Areas.ChartLED>
+              <Card>
+                <div style={{ textAlign: 'center', marginBottom: '1em' }}>
+                  <b>LED State</b>
+                </div>
+                <ChartContainer>
+                  <LineChart key="led" dataSource={ledStateDataSource} />
+                  <RealTimeDomain window={10000} />
+                  <TimeAxis />
+                  <VerticalAxis />
+                </ChartContainer>
+              </Card>
+            </Areas.ChartLED>
+
             <Areas.Light>
-              <LightBulb
-                containerStyle={{ margin: '20px auto', width: '80%' }}
-                width="40vw"
-              />
-            </Areas.Light> */}
+              <LightBulb containerStyle={{ margin: '20px auto', width: '80%' }} width="40vw" />
+            </Areas.Light>
 
             <Areas.Slider>
               <Card>
