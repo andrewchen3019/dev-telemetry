a20334c63365de6cb2def53638db2ae7f8d7f001
Anya Krislav <anya.krislav@duke.edu>
2025-11-10 01:36:41 -0500

    add propulsion button; fix structure and imports

--- src/application/components/Data/index.tsx
+++ src/application/components/Data/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useMemo } from 'react'
 import { useHardwareState } from '@electricui/components-core'
 
 export const Data = () =>  {
--- src/application/pages/DevicePages/OverviewPage.tsx
+++ src/application/pages/DevicePages/OverviewPage.tsx
@@ -8,23 +8,36 @@ import {
 
 import { Card } from '@blueprintjs/core'
 import { Composition } from 'atomic-layout'
-import { IntervalRequester } from '@electricui/components-core'
+import { IntervalRequester, useDeviceManager } from '@electricui/components-core'
 import { LightBulb } from '../../components/LightBulb'
-// import { Data } from '../../components/Data'
+import { Data } from '../../components/Data'
 import { useMessageDataSource } from '@electricui/core-timeseries'
 import React from 'react'
 import { RouteComponentProps } from '@reach/router'
 import { Slider } from '@electricui/components-desktop-blueprint'
 
+import { Button} from '@electricui/components-desktop-blueprint'
+import { ButtonGroup } from '@blueprintjs/core'
+
 const layoutDescription = `
   ChartSpeed ChartBattery ChartLED
   Light      Slider       Slider
+  Button
 `
+const deviceManager = useDeviceManager() as any     // cast to 'any' to reach internals
+  const device =
+    deviceManager.connectedDevices?.[0] ??
+    deviceManager.devices?.[0] ??
+    null
+
+  const propulsionOn = () => device?.write({ propulsion: 1 })
+  const propulsionOff = () => device?.write({ propulsion: 0 })
 
 export const OverviewPage = (props: RouteComponentProps) => {
   const ledStateDataSource = useMessageDataSource('led_state');
   const batteryDataSource = useMessageDataSource('battery');
   const speedDataSource = useMessageDataSource('speed');
+  
   return (
     <React.Fragment>
       <IntervalRequester interval={50} messageIDs={['led_state','battery','speed']} />
@@ -32,6 +45,8 @@ export const OverviewPage = (props: RouteComponentProps) => {
       <Composition areas={layoutDescription} gap={10} autoCols="1fr">
         {Areas => (
           <React.Fragment>
+            
+             {/* DISPLAYS SPEED */}
             <Areas.ChartSpeed>
               <Card>
                 <div style={{ textAlign: 'center', marginBottom: '1em' }}>
@@ -46,6 +61,7 @@ export const OverviewPage = (props: RouteComponentProps) => {
               </Card>
             </Areas.ChartSpeed>
 
+            {/* DISPLAYS BATTERY EFFICIENCY */}
             <Areas.ChartBattery>
               <Card>
                 <div style={{ textAlign: 'center', marginBottom: '1em' }}>
@@ -60,6 +76,7 @@ export const OverviewPage = (props: RouteComponentProps) => {
               </Card>
             </Areas.ChartBattery>
 
+           {/* DISPLAYS LED STATE */}
             <Areas.ChartLED>
               <Card>
                 <div style={{ textAlign: 'center', marginBottom: '1em' }}>
@@ -78,6 +95,7 @@ export const OverviewPage = (props: RouteComponentProps) => {
               <LightBulb containerStyle={{ margin: '20px auto', width: '80%' }} width="40vw" />
             </Areas.Light>
 
+          {/* SLIDER FOR LED FREQUENCY */}
             <Areas.Slider>
               <Card>
                 <div style={{ margin: 20 }}>
@@ -93,6 +111,27 @@ export const OverviewPage = (props: RouteComponentProps) => {
                 </div>
               </Card>
             </Areas.Slider>
+
+            {/* BUTTON TO CONTROL PROPULSION SWITCH */}
+            {/* NOTE: PERHAPS SWITCH TO SLIDER FOR BETTER CONTROL? */}
+            <Areas.Button>
+              <Card>
+              <div >
+                <ButtonGroup>
+                  <Button intent="success" large onClick={propulsionOn}>
+                      Propulsion On
+                  </Button>
+                  
+                  <Button intent="danger"  large onClick = {propulsionOff}>
+                      Propulsion Off 
+                  </Button>
+
+                </ButtonGroup>
+
+              </div>
+              </Card>
+            </Areas.Button>
+
           </React.Fragment>
         )}
       </Composition>
